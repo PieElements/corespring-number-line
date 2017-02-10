9a9d5e7c5ab7dd66d50e72096da55624e89c980d
Ed Eustace <ed.eustace@gmail.com>
2017-02-10 17:22:03 +0000

    tweaks"

--- src/point-groupings.jsx
+++ src/point-groupings.jsx
@@ -4,12 +4,24 @@ export default class PointGroupings extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
+  choosePoint() {
+    this.choose('point');
+  }
+  choose(t) {
+    console.log('choose: ', t);
+    if (this.props.onChoose) {
+      this.props.onChoose(t);
+
+    }
+  }
 
   render() {
-    return <ul className="nav nav-pills">
+    return <ul className="point-groupings">
       <li role="presentation"
+        onClick={this.choose.bind(this, 'point')}
       //ng-show="isGroupEnabled(\'Point\')" ng-class="{active: isGroupActive(\'Point\')}" ng-mousedown="selectGroup(\'Point\')"
-      ><a>Point</a></li>
+      ><a
+      >Point</a></li>
       <li role="presentation"
       //ng-show="isGroupEnabled(\'Line\')" ng-class="{active: isGroupActive(\'Line\')}" ng-mousedown="selectGroup(\'Line\')"
       ><a>Line</a></li>
