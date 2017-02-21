547e34e57c5ae81634437ed838a34c5577f149bb
Ed Eustace <ed.eustace@gmail.com>
2017-02-21 16:43:56 +0000

    add transitions

--- src/number-line/feedback.jsx
+++ src/number-line/feedback.jsx
@@ -5,6 +5,7 @@ import PartiallyCorrectIcon from 'corespring-icon/partially-correct-icon';
 import ShowRationaleIcon from 'corespring-icon/show-rationale-icon';
 import IncorrectIcon from 'corespring-icon/incorrect-icon';
 import classNames from 'classnames';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 require('./feedback.less');
 
@@ -25,10 +26,21 @@ const Feedback = (props) => {
   let className = classNames('feedback', props.type);
   let Icon = getIcon(props.type);
 
-  return <div className={className} style={{ width: props.width }}>
-    <Icon iconSet="emoji" shape="square" />
-    <span className="message" dangerouslySetInnerHTML={{ __html: props.message }}></span>
-  </div>;
+  return <ReactCSSTransitionGroup
+    transitionName="fb"
+    transitionAppear={true}
+    transitionAppearTimeout={300}
+    transitionEnterTimeout={300}
+    transitionLeaveTimeout={300}>
+    <div key="panel"
+      className={className}
+      style={{ width: props.width }}>
+      <Icon iconSet="emoji" shape="square" />
+      <span
+        className="message"
+        dangerouslySetInnerHTML={{ __html: props.message }}></span>
+    </div>
+  </ReactCSSTransitionGroup>;
 }
 
 export default Feedback;
