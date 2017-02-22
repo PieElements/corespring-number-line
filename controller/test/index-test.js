0c5ec49b5aa468e5522a34f36ab3911afe26fe8f
Ed Eustace <ed.eustace@gmail.com>
2017-02-22 14:57:14 +0000

    controller test (wip)

--- controller/test/index-test.js
+++ controller/test/index-test.js
@@ -1,7 +1,37 @@
-const { expect } = require('chai');
-
+import chai from 'chai';
+const { expect } = chai;
+chai.use(require('chai-shallow-deep-equal'));
+import _ from 'lodash';
 describe('controller', () => {
-  it('..', () => {
-    expect(true).to.be.true;
-  })
+
+  let controller;
+
+  let assertModel = (question, session, env, expected) => {
+    question = _.merge(question, {
+      model: {
+        config: {}
+      }
+    });
+    session = _.merge(session, {});
+    env = _.merge(env, {});
+
+    it('creates model', () => {
+      return controller.model(question, session, env)
+        .then(o => {
+          console.log('result: ', o);
+          expect(o).to.shallowDeepEqual(expected);
+        })
+        .catch(e => {
+          console.log(e);
+          throw new Error('Create Model spec error')
+        })
+
+    });
+  }
+
+  beforeEach(() => {
+    controller = require('../index');
+  });
+
+  assertModel({}, {}, {}, {});
 });
