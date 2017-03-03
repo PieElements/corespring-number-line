f69fafa7678bf71acd3b9b90e7d49230651d5659
Ed Eustace <ed.eustace@gmail.com>
2017-03-03 17:52:55 +0000

    rename to configure

--- configure/src/index.js
+++ configure/src/index.js
@@ -1,11 +1,11 @@
 export default class CorespringNumberLineConfiguration extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `<div>corespring number line config pane</div>`;
+    this.innerHTML = `<h1>...test.....</h1>`;
   }
 
   set model(m) {
     this._model = m;
-    console.log('this._model: ', this._model);
+    console.log('this._modkel: ', this._model);
   }
 }
